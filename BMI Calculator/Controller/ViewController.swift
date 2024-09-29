f9208eac1818c7a560f29c1e7926602dde030d1b
murad <muradkv@gmail.com>
2024-09-29 05:27:22 +0300

    Initial commit

---
+++ BMI Calculator/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by murad on 29.09.2024.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
